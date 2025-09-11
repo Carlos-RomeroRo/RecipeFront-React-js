32c72002226370865b8e260b7314b3e4197b4ecc
Carlos Romero <romero.ca20042018@gmail.com>
2025-09-11 11:35:08 -0500

    add: create functions and complete footer

--- recipes-api/src/components/Footer.jsx
+++ recipes-api/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-
+import ContactButton from '../functions/contactButton'
 function Footer() {
   return (
         <footer className="bg-red-500 shadow-sm w-full">
@@ -18,17 +18,21 @@ function Footer() {
 
                             <li className="hover:text-blue-600 me-3">
                                 <a href="https://github.com/Carlos-RomeroRo" target="_blank">Github</a> 
+                                <a href="https://github.com/Carlos-RomeroRo">
                                     <svg  className="inline mr-3 ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+                                </a>
+                            
                             </li>
                             <li className="me-3 hover:text-blue-600">
-                                <a  href="https://www.linkedin.com/in/carlos-romero-626b08287/?midSig=1Z1kJh0-MLIHU1&eid=jcqzcu-mewyzutr-tf&midToken=AQG3u-MiUKDL8A&trkEmail=eml-security_two_step_verification_login_attempt-header-0-profile_glimmer-null-jcqzcu%7Emewyzutr%7Etf-null-null&trk=eml-security_two_step_verification_login_attempt-header-0-profile_glimmer&originalSubdomain=co" target="_blank">Linkedin</a>
-                                <svg className="inline mr-3 ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+                                <a  href="https://www.linkedin.com/in/carlos-romero-626b08287/" target="_blank">Linkedin</a>
+                                    <a href="https://www.linkedin.com/in/carlos-romero-626b08287/" target="_blank">
+                                            <svg className="inline mr-3 ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+                                    </a>
                             </li>
                             <li className="me-3 hover:text-blue-600">
-                                <a href="hhttps://mail.google.com/mail/u/0/#inbox" 
-                                    target="_blank" 
-                                >Gmail</a>
-                                <svg href="hhttps://mail.google.com/mail/u/0/#inbox"  className="inline mr-3 ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-plus-icon lucide-mail-plus"><path d="M22 13V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h8"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/><path d="M19 16v6"/><path d="M16 19h6"/></svg>
+                                <ContactButton>
+
+                                </ContactButton>
                             </li>
                         </ul>
             </div>
